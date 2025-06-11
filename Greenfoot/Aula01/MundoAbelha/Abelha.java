8e0caafdbd1fcf8c97ea97ef7a34fa2b4fbfabc9
Castelucci21 <laracastelucci21@gmail.com>
2025-06-10 21:06:42 -0300

    Greenfoot terminado

--- Greenfoot/Aula01/MundoAbelha/Abelha.java
+++ Greenfoot/Aula01/MundoAbelha/Abelha.java
@@ -11,10 +11,11 @@ public class Abelha extends Actor
     
     //Definindo os fields
     private int vidas;
-    private int score;
-    private int PONTOS = 100;
+    //private int score;
+    //private int PONTOS = 100;
     private int indice;
     private GreenfootImage imgs[];
+    
     
     //Definindo o constructor
     /**
@@ -22,7 +23,6 @@ public class Abelha extends Actor
      */
     public Abelha(){
         vidas = 3; //vai ter 3 vidas
-        score = 0;
         //GreenfootImage img = new GreenfootImage("bee01.png");
         //setImage(img);
         
@@ -51,6 +51,7 @@ public class Abelha extends Actor
         verificarPosicao();
         capturarMosca();
         serCapturadoPelaAranha();
+        atualizarVidas();
     }
     /**
      * Método que verifica posição da Abelha.
@@ -102,7 +103,10 @@ public class Abelha extends Actor
         if (isTouching(Mosca.class)) {
             removeTouching(Mosca.class);
             Greenfoot.playSound("slurp.wav");
-            atualizarScore();
+            
+            //Fazendo o casting para Beeworld
+            ((BeeWorld)getWorld()).updateScore();
+            
             //Vai de 1 a 800
             int posX = Greenfoot.getRandomNumber(
                         getWorld().getWidth()) + 1;
@@ -142,16 +146,19 @@ public class Abelha extends Actor
                 getWorld().showText("GAME OVER", 400, 300);
                 Greenfoot.stop();
             }
-            getWorld().showText("Vidas: " + vidas, 100, 30);
+           // getWorld().showText("Vidas: " + vidas, 100, 30);
     }
 
-    public void atualizarScore(){
-        score += PONTOS; //score = score + PONTOS
-        getWorld().showText("Score: " + score, 100, 10);
-    }
+    //public void atualizarScore(){
+        //score += PONTOS; //score = score + PONTOS
+       // getWorld().showText("Score: " + score, 100, 10);
+    //}
     
     public void animarAbelha(){
         indice = (indice + 1) % 4;
         setImage(imgs[indice]);
+    }
+    public void atualizarVidas(){
+        getWorld().showText("Vidas: "+ vidas, 700,10);
     }
 }
--- Greenfoot/Aula01/MundoAbelha/BeeWorld.java
+++ Greenfoot/Aula01/MundoAbelha/BeeWorld.java
@@ -9,6 +9,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class BeeWorld extends World
 {
 
+    // Fields 
+    private int score;
+    private final int PONTOS = 100;
+    private Abelha abelha;
     /**
      * Constructor for objects of class BeeWorld.
      * 
@@ -18,6 +22,7 @@ public class BeeWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
         prepare();
+        score = 0;
     }
     
     /**
@@ -29,9 +34,9 @@ public class BeeWorld extends World
         Aranha aranha = new Aranha();
         addObject(aranha,33,39);
         
-        Abelha abelha = new Abelha();
+        abelha = new Abelha();
         addObject(abelha,41,182);
-        
+    
         for(int qtd=0;qtd<20;qtd++){
             int posX = Greenfoot.getRandomNumber(800);
             int posY = Greenfoot.getRandomNumber(600);
@@ -39,5 +44,26 @@ public class BeeWorld extends World
             int rol = Greenfoot.getRandomNumber(360)+1;
             addObject(new Mosca(vel,rol),posX,posY);
         }
+        
+        //mostrando texto
+        TextoColorido txt = new TextoColorido(
+         "Mensagem teste",500,50,18,Color.BLUE,Color.GRAY);
+         
+         addObject(txt,500,50);
+    }
+    // Acessador ou getter do Score
+    public int getScore(){
+        return score;
+    }
+    /**
+     * Método para atualizar o Score da Abelha
+     */
+    public void updateScore(){
+        score += PONTOS; // score = score + PONTOS
+        showText("Score: " + score,100,10);
+    }
+    //Acessador ou getter da Abelha
+    public Abelha getAbelha(){
+        return abelha;
     }
 }
