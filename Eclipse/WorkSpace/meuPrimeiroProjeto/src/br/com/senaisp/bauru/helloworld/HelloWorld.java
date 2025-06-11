ce7374d54abcc1c182193b4c48fc61491cd925c2
Castelucci21 <laracastelucci21@gmail.com>
2025-06-10 21:56:12 -0300

    Eclipse

---
+++ Eclipse/WorkSpace/meuPrimeiroProjeto/src/br/com/senaisp/bauru/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package br.com.senaisp.bauru.helloworld;
+
+public class HelloWorld {
+	
+	public static void main(String[]args ) {
+		//TODO auto-generated method stub
+		System.out.println("Olá Mundo!!");
+	}
+
+}
