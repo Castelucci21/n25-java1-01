37469767c08f3326807ca28aca164c278f9e9aa8
Castelucci21 <laracastelucci21@gmail.com>
2025-06-23 21:54:21 -0300

    Cartas

---
+++ Eclipse/WorkSpace/br.com.senaisp.com.Aula03/src/br/com/senaisp/com/Aula06/exemplo08.java
@@ -0,0 +1,36 @@
+package br.com.senaisp.com.Aula06;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class exemplo08 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int idade;
+		do {
+			
+		try {
+			System.out.println("Digite sua idade: ");
+			idade = sc.nextInt();
+			System.out.println("Você nasceu em " + (2025 - idade));
+		} catch (InputMismatchException e) {
+			// TODO Auto-generated catch block
+			idade = 0;
+			System.out.println("Você digitou uma idade inválida!");
+			sc.nextLine(); //Caputra o valor com erro para não ficar em loop
+			}catch (ArithmeticException e) {
+				idade = 0;
+				System.out.println("Divisão por zero!");
+			}
+			catch (Exception e) {
+				idade = 0;
+				System.out.println("Erro geral");
+			}
+		
+		} while (idade<=0);
+		
+		sc.close();
+	}
+
+}
